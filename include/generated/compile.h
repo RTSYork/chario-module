58ffdad90c1ef941e1adcc996e0574dc01821526
Russell Joyce <russell.joyce@york.ac.uk>
2016-05-12 17:04:31 +0100

    Initial commit of basic test character device module Includes linux-4.1.15-rt17 headers to make CLion happy

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT RT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT RT Sat Apr 30 21:03:18 BST 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "zynq-mitx"
+#define LINUX_COMPILER "gcc version 4.9.2 (Debian 4.9.2-10) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.1.15-rt17-profiling-g8a01727"
