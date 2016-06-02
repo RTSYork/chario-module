1a6546ef8fe8af2744e343ae363a6156384f20d1
Russell Joyce <russell.joyce@york.ac.uk>
2016-06-02 14:59:33 +0100

    We now request nblocks-1 from the NVMe driver, which seems to work NVMe driver adds 1 to nblocks, for some reason. It's possible that it had a good reason to, and that this will come back to bite me. Also properly bases nblocks on the device block size, and adds debug message.

--- charfs.c
+++ charfs.c
@@ -178,14 +178,14 @@ static int submit_io_test(void) {
 static int submit_user_io(char *buffer, size_t len, __u8 command) {
 	struct nvme_dev *dev = charfs_nvme_get_current_dev();
 	struct nvme_ns *ns = list_first_entry(&dev->namespaces, struct nvme_ns, list);
-	__u16 blocks = (__u16)DIV_ROUND_UP(len, 4096);
+	__u16 blocks = (__u16)((len >> ns->lba_shift) - 1);
 	int result;
 
 	struct nvme_user_io uio = {
 		.opcode = command,			// (__u8)  Read command
 		.flags = 0,				// (__u8)  No flags?
 		.control = 0,				// (__u16) ?
-		.nblocks = blocks,			// (__u16) 1 block (4096 bytes)
+		.nblocks = blocks,			// (__u16) Number of blocks ((length / lba size) - 1)
 		.rsvd = 0,				// (__u16) ?
 		.metadata = 0,				// (__u64) ?
 		.addr = (__u64)(uintptr_t)buffer,	// (__u64) Buffer address
@@ -196,7 +196,7 @@ static int submit_user_io(char *buffer, size_t len, __u8 command) {
 		.appmask = 0				// (__u16) ?
 	};
 
-	printk(KERN_DEBUG "CharFS: submit_user_io()");
+	printk(KERN_DEBUG "CharFS: submit_user_io() nblocks: %hu, addr: 0x%08x, opcode: %hhu", blocks, (unsigned int)buffer, command);
 
 	result = charfs_nvme_submit_io_user(ns, &uio);
 
