ac95bb12c7c3a92ea12e0c52950269b89475da67
Russell Joyce <russell@russellj.co.uk>
2016-11-06 19:35:50 +0000

    Added simple test for reading and writing data in three tasks

--- libchario/libchario-test.c
+++ libchario/libchario-test.c
@@ -1,22 +1,70 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "libchario.h"
 
+struct chario_task task1;
+struct chario_task task2;
+struct chario_task task3;
 
-int main(int argc, char *argv[]) {
+struct chario_task *tasks[] = {&task1, &task2, &task3};
 
-	printf("libchario test\n");
+char *buffer;
 
-	chario_init_device();
+int returnVal = 0;
 
-	struct chario_task task;
+int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused))) {
 
-	chario_init_task(&task, 0);
+	printf("\n** libchario test started **\n\n");
 
-	chario_add_range_to_task(&task, 1, 0);
-	chario_add_range_to_task(&task, 2, 50);
-	chario_add_range_to_task(&task, 32, 100);
+	buffer = chario_init_device();
 
-	chario_read_blocks_for_task(&task);
+	chario_init_task(&task1, 1);
+	chario_add_range_to_task(&task1, 1, 0);
+	chario_add_range_to_task(&task1, 2, 50);
+	chario_add_range_to_task(&task1, 32, 100);
+	chario_flush_blocks_for_task(&task1);
 
-	return 0;
+	chario_init_task(&task2, 2);
+	chario_add_range_to_task(&task2, 131072, 100000);
+	chario_flush_blocks_for_task(&task2);
+
+	chario_init_task(&task3, 3);
+	chario_add_range_to_task(&task3, 65536, 256);
+	chario_add_range_to_task(&task3, 32, 300000);
+	chario_flush_blocks_for_task(&task3);
+
+
+	printf("\n** Tasks initialised **\n\n");
+
+
+	for (int i = 0; i < 10; i++) {
+
+		printf("\n** Iteration %d **\n\n", i);
+
+		for (int j = 0; j < 3; j++) {
+			chario_load_blocks_for_task(tasks[j]);
+
+			for (size_t k = 0; k < tasks[j]->total_size; k++) {
+				if (buffer[k] == i) {
+					buffer[k]++;
+				}
+				else {
+					printf("Byte 0x%08x is incorrect (0x%02x)\n", (unsigned)&buffer[k], buffer[k]);
+					returnVal = 1;
+					goto end;
+				}
+			}
+
+			chario_flush_blocks_for_task(tasks[j]);
+
+			sleep(1);
+		}
+	}
+
+
+end:
+	chario_close_device();
+
+	return returnVal;
 }
