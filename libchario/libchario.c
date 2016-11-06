4afc692206a085af345b2de33b25097837e9e725
Russell Joyce <russell@russellj.co.uk>
2016-11-06 19:31:49 +0000

    Implemented basic tasks with block ranges in libchario Supports loading and flushing block ranges to disk

--- libchario/libchario.c
+++ libchario/libchario.c
@@ -7,14 +7,27 @@
 #include <unistd.h>
 #include "libchario.h"
 
-int fd;
+int fd = -1;
+char *buffer;
 
 
-int chario_init_device(void) {
-	fd = open("/dev/chardisk0", O_RDWR);
-
+char *chario_init_device(void) {
 	dbg_print("chario_init_device");
+	fd = open("/dev/chardisk0", O_RDWR);
+	dbg_print("    fd = %d", fd);
+	buffer = aligned_alloc(CHARIO_BLOCK_SIZE, 512 * 1024*1024);
+	memset(buffer, 0, 512 * 1024*1024);
+	dbg_print("    buffer = 0x%08x", (unsigned)buffer);
+	return buffer;
+}
 
+
+int chario_close_device(void) {
+	dbg_print("chario_close_device");
+	close(fd);
+	fd = -1;
+	free(buffer);
+	buffer = 0;
 	return 0;
 }
 
@@ -27,28 +40,28 @@ int chario_init_task(struct chario_task *task, int id) {
 }
 
 
-int chario_add_range_to_task(struct chario_task *task, size_t size, off_t offset) {
+int chario_add_range_to_task(struct chario_task *task, size_t count, off_t offset) {
 	struct chario_blocks_range *range;
 	struct chario_blocks_range *prev_item;
 
-	dbg_print("chario_add_range_to_task(%d, %zu, %jd)", task->id, size, (intmax_t)offset);
+	dbg_print("chario_add_range_to_task(%d, %zu, %jd)", task->id, count, (intmax_t)offset);
 
 	range = malloc(sizeof(struct chario_blocks_range));
 
 	range->start = offset;
-	range->count = size;
+	range->count = count;
 
 	if (list_empty(&task->ranges)) {
 		range->buffer_offset = 0;
-		dbg_print("    first item");
 	}
 	else {
 		prev_item = list_last_entry(&task->ranges, struct chario_blocks_range, list);
 		range->buffer_offset = (off_t)(prev_item->buffer_offset + (prev_item->count * CHARIO_BLOCK_SIZE));
-		dbg_print("    not first item");
 	}
 
 	list_add_tail(&range->list, &task->ranges);
+	task->range_count++;
+	task->total_size += (range->count * CHARIO_BLOCK_SIZE);
 
 	dbg_print("    range->buffer_offset = %jd", (intmax_t)range->buffer_offset);
 
@@ -60,11 +73,17 @@ int chario_add_range_to_task(struct chario_task *task, size_t size, off_t offset
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "CannotResolve"
 #endif
-int chario_read_blocks_for_task(struct chario_task *task) {
+int chario_load_blocks_for_task(struct chario_task *task) {
 	struct chario_blocks_range *range;
 	int i = 0;
+	ssize_t count;
 
-	dbg_print("chario_read_blocks_for_task(%d)", task->id);
+	info_print("chario_load_blocks_for_task(%d)", task->id);
+
+	if (fd == -1 || buffer == 0) {
+		dbg_print("    Device not initialised");
+		return 1;
+	}
 
 	list_for_each_entry(range, &task->ranges, list) {
 		dbg_print("    item %d", i++);
@@ -72,6 +91,38 @@ int chario_read_blocks_for_task(struct chario_task *task) {
 		dbg_print("        range->start = %jd", (intmax_t)range->start);
 		dbg_print("        range->count = %zu", range->count);
 		dbg_print("        range->buffer_offset = %jd", (intmax_t)range->buffer_offset);
+
+		lseek(fd, range->start * CHARIO_BLOCK_SIZE, SEEK_SET);
+		count = read(fd, buffer + range->buffer_offset, range->count * CHARIO_BLOCK_SIZE);
+		info_print("        read %zd of %zu bytes to 0x%08x", count, range->count * CHARIO_BLOCK_SIZE, (unsigned)(buffer + range->buffer_offset));
+	}
+
+	return 0;
+}
+
+
+int chario_flush_blocks_for_task(struct chario_task *task) {
+	struct chario_blocks_range *range;
+	int i = 0;
+	ssize_t count;
+
+	info_print("chario_flush_blocks_for_task(%d)", task->id);
+
+	if (fd == -1 || buffer == 0) {
+		dbg_print("    Device not initialised");
+		return 1;
+	}
+
+	list_for_each_entry(range, &task->ranges, list) {
+		dbg_print("    item %d", i++);
+		dbg_print("    range = 0x%08x", (unsigned)range);
+		dbg_print("        range->start = %jd", (intmax_t)range->start);
+		dbg_print("        range->count = %zu", range->count);
+		dbg_print("        range->buffer_offset = %jd", (intmax_t)range->buffer_offset);
+
+		lseek(fd, range->start * CHARIO_BLOCK_SIZE, SEEK_SET);
+		count = write(fd, buffer + range->buffer_offset, range->count * CHARIO_BLOCK_SIZE);
+		info_print("        wrote %zd of %zu bytes from 0x%08x", count, range->count * CHARIO_BLOCK_SIZE, (unsigned)(buffer + range->buffer_offset));
 	}
 
 	return 0;
@@ -79,3 +130,12 @@ int chario_read_blocks_for_task(struct chario_task *task) {
 #ifdef __clang__
 #pragma clang diagnostic pop
 #endif
+
+char *buffer_for_range(struct chario_blocks_range *range) {
+	info_print("buffer_for_range(%jd)",(intmax_t)range->start);
+
+	if (buffer == 0)
+		return 0;
+
+	return buffer + range->buffer_offset;
+}
--- libchario/libchario.h
+++ libchario/libchario.h
@@ -5,7 +5,8 @@
 #include <sys/types.h>
 #include "list.h"
 
-#define LIBCHARIO_DBG
+//#define LIBCHARIO_DBG
+#define LIBCHARIO_INFO
 
 #define CHARIO_BLOCK_SIZE 4096
 
@@ -13,6 +14,8 @@
 struct chario_task {
     int id;
     struct list_head ranges;
+    unsigned range_count;
+    size_t total_size;
 };
 
 struct chario_blocks_range {
@@ -23,17 +26,28 @@ struct chario_blocks_range {
 };
 
 
-int chario_init_device(void);
+char *chario_init_device(void);
+int chario_close_device(void);
 
 int chario_init_task(struct chario_task *task, int id);
 
-int chario_add_range_to_task(struct chario_task *task, size_t size, off_t offset);
+int chario_add_range_to_task(struct chario_task *task, size_t count, off_t offset);
 
-int chario_read_blocks_for_task(struct chario_task *task);
+int chario_load_blocks_for_task(struct chario_task *task);
+
+int chario_flush_blocks_for_task(struct chario_task *task);
+
+char *buffer_for_range(struct chario_blocks_range *range);
 
 
-#ifdef LIBCHARIO_DBG
+#if defined(LIBCHARIO_DBG)
 #define dbg_print(format, ...) printf("libchario: " format "\n", ##__VA_ARGS__)
+#else
+#define dbg_print(format, args...) {}
+#endif
+
+#if defined(LIBCHARIO_INFO) || defined(LIBCHARIO_DBG)
+#define info_print(format, ...) printf("libchario: " format "\n", ##__VA_ARGS__)
 #else
 #define dbg_print(format, args...) {}
 #endif
