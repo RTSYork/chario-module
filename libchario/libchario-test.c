a49ce2fa4de33a14804400038e55574e390dfec7
Russell Joyce <russell@russellj.co.uk>
2016-11-06 21:26:03 +0000

    Improved test so different tasks can be differentiated

--- libchario/libchario-test.c
+++ libchario/libchario-test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include "libchario.h"
 
 struct chario_task task1;
@@ -23,30 +24,33 @@ int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)
 	chario_add_range_to_task(&task1, 1, 0);
 	chario_add_range_to_task(&task1, 2, 50);
 	chario_add_range_to_task(&task1, 32, 100);
-	chario_flush_blocks_for_task(&task1);
 
 	chario_init_task(&task2, 2);
 	chario_add_range_to_task(&task2, 131072, 100000);
-	chario_flush_blocks_for_task(&task2);
 
 	chario_init_task(&task3, 3);
 	chario_add_range_to_task(&task3, 65536, 256);
 	chario_add_range_to_task(&task3, 32, 300000);
-	chario_flush_blocks_for_task(&task3);
+
+	for (uint8_t j = 0; j < 3; j++) {
+		for (size_t k = 0; k < tasks[j]->total_size; k++) {
+			buffer[k] = j << 4;
+		}
+		chario_flush_blocks_for_task(tasks[j]);
+	}
 
 
 	printf("\n** Tasks initialised **\n\n");
 
 
-	for (int i = 0; i < 10; i++) {
-
+	for (uint8_t i = 0; i < 10; i++) {
 		printf("\n** Iteration %d **\n\n", i);
 
-		for (int j = 0; j < 3; j++) {
+		for (uint8_t j = 0; j < 3; j++) {
 			chario_load_blocks_for_task(tasks[j]);
 
 			for (size_t k = 0; k < tasks[j]->total_size; k++) {
-				if (buffer[k] == i) {
+				if (buffer[k] == (j << 4 | i)) {
 					buffer[k]++;
 				}
 				else {
@@ -57,8 +61,6 @@ int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)
 			}
 
 			chario_flush_blocks_for_task(tasks[j]);
-
-			sleep(1);
 		}
 	}
 
